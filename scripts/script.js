80bacc8d7d7156bf47317112a3d6279fbb3660ca
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-09-24 14:11:09 -0400

    adding style to all h1 in JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
-let search = document.getElementById('search')
+let search = document.getElementById('search');
 
 search.style.textAlign = 'right';
+
+// styling all h1 elements
+let headingOne = document.querySelectorAll('h2');
+
+headingOne.forEach(heading => {
+    heading.style.backgroundColor = '#6f6dac'
+    heading.style.color = 'white'
+    heading.style.padding = '8px 0px 8px 25px';
+    heading.style.borderRadius = '10px'
+})
+
+
+
+
+
+
+
+
+
+
+
+
